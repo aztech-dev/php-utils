56b5cdd73186579f48023bf203d423b9759b7216
aztech-dev <aztech-dev@users.noreply.github.com>
2014-07-30 19:38:34 +0200

    Update TypedCollection.php

--- src/Collections/TypedCollection.php
+++ src/Collections/TypedCollection.php
@@ -19,8 +19,20 @@ class TypedCollection extends TypedIterator
     public function addObject($item)
     {
         $this->validate($item);
-
         $this->items[] = $item;
+    }
+    
+    /**
+     *
+     * @param mixed $item
+     * @throws \InvalidArgumentException
+     */
+    public function addRange(array $items)
+    {
+        foreach ($items as $item) {
+            $this->validate($item);
+            $this->items[] = $item;
+        }
     }
 
     /**
@@ -32,12 +44,10 @@ class TypedCollection extends TypedIterator
     {
         $this->validate($item);
 
-        if (in_array($item, $this->items)) {
-            $this->items = array_udiff($this->items, array(
-                $item
-            ), function ($a, $b) {
-                return $a === $b;
-            });
-        }
+        $this->items = array_udiff($this->items, array(
+            $item
+        ), function ($a, $b) {
+            return $a === $b;
+        });
     }
 }
