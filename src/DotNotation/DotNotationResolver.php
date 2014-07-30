5d6f31ce0dee0a15047187b7135bd5bf49da3b07
Thibaud Fabre <thibaud@aztech.io>
2014-07-30 17:01:16 +0200

    "Extract method" refactorings

--- src/DotNotation/DotNotationResolver.php
+++ src/DotNotation/DotNotationResolver.php
@@ -9,40 +9,49 @@ namespace Aztech\Util\DotNotation;
  */
 class DotNotationResolver
 {
-    
+
     public static function resolve($value, $name, $default = null)
     {
         if (! DotNotationParser::hasDot($name)) {
-            if (is_object($value) && isset($value->{$name})) {
-                return $value->{$name};
-            }
-            elseif (is_array($value) && array_key_exists($name, $value)) {
-                return $value[$name];
-            }
-            
-            return $default;
+            return self::getDirectProperty($value, $name, $default);
         }
-        
+
         $elements = DotNotationParser::getComponents($name, 2);
         $firstLevelObject = self::resolve($value, $elements[0]);
-        
+
         return self::resolve($firstLevelObject, $elements[1]);
     }
-    
+
     public static function propertyOrIndexExists($value, $name) {
         if (! DotNotationParser::hasDot($name)) {
-            if (is_object($value)) {
-                return isset($value->{$name});
-            }
-            elseif (is_array($value) || $value instanceof \ArrayAccess) {
-                return isset($value[$name]);
-            }
+            return self::checkDirectProperty($value, $name);
         }
-        
+
         $elements = DotNotationParser::getComponents($name, 2);
         $firstLevelObject = self::resolve($value, $elements[0]);
-        
+
         return self::propertyOrIndexExists($firstLevelObject, $elements[1]);
     }
-    
+
+    private static function checkDirectProperty($value, $name)  {
+        if (is_object($value)) {
+            return isset($value->{$name});
+        }
+        elseif (is_array($value) || $value instanceof \ArrayAccess) {
+            return isset($value[$name]);
+        }
+    }
+
+    private static function getDirectProperty($value, $name, $default)
+    {
+        if (is_object($value) && isset($value->{$name})) {
+            return $value->{$name};
+        }
+        elseif (is_array($value) && array_key_exists($name, $value)) {
+            return $value[$name];
+        }
+
+        return $default;
+    }
+
 }
--- src/Mapping/ObjectMapper.php
+++ src/Mapping/ObjectMapper.php
@@ -7,55 +7,60 @@ use \Aztech\Util\DotNotation\DotNotationResolver;
 
 class ObjectMapper
 {
-    
+
     private $mappings = array();
-    
+
     private $constants = array();
-    
+
     public function addMapping($sourceKey, $targetKey, $required = false)
     {
-        $this->mappings[$sourceKey] = array('target' => $targetKey, 'req' => $required); 
+        $this->mappings[$sourceKey] = array('target' => $targetKey, 'req' => $required);
     }
-    
+
     public function addConstantMapping($targetKey, $value)
     {
         $this->constants[$targetKey] = $value;
     }
-    
+
     public function map($source, & $target)
     {
         foreach ($this->mappings as $sourceKey => $targetData) {
-            $targetKey = $targetData['target'];
-            $required = $targetData['req'];
-            $exists = $this->hasPropertyOrIndex($source, $sourceKey);
-            
-            if (! $required && ! $exists) {
-                continue;
-            }
-            
-            if (! $exists) {
-                throw new \InvalidArgumentException('Source has no such property or index : "' . $sourceKey . "'");
-            }
-            
-            $this->assign($target, $targetKey, $this->read($source, $sourceKey));
-        }   
-        
+            $this->mapItem($source, $target, $sourceKey, $targetData);
+        }
+
         foreach ($this->constants as $targetKey => $value) {
             $this->assign($target, $targetKey, $value);
         }
     }
-    
-    private function hasPropertyOrIndex($object, $key) 
+
+    private function mapItem(& $source, & $target, $sourceKey, array & $targetData)
+    {
+        $targetKey = $targetData['target'];
+        $required = $targetData['req'];
+        $exists = $this->hasPropertyOrIndex($source, $sourceKey);
+
+        if (! $required && ! $exists) {
+            continue;
+        }
+
+        if (! $exists) {
+            throw new \InvalidArgumentException('Source has no such property or index : "' . $sourceKey . "'");
+        }
+
+        $this->assign($target, $targetKey, $this->read($source, $sourceKey));
+    }
+
+    private function hasPropertyOrIndex($object, $key)
     {
         return DotNotationResolver::propertyOrIndexExists($object, $key);
     }
-    
+
     private function read($source, $key)
     {
         return DotNotationResolver::resolve($source, $key);
     }
-    
-    private function assign($target, $targetKey, $value) 
+
+    private function assign($target, $targetKey, $value)
     {
         if (is_array($target)) {
             $target[$targetKey] = $value;
