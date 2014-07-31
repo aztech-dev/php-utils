7987bec66ac59da05bce09241b8963d1371dde13
Thibaud Fabre <thibaud@aztech.io>
2014-07-31 04:34:42 +0200

    Add generic iterator

---
+++ src/Collections/StandardIterator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aztech\Util\Collections;
+
+class StandardIterator implements \Iterator
+{
+
+    protected $items = array();
+
+    public function __construct(array $items = array())
+    {
+        $this->items = $items;
+    }
+
+    public function rewind()
+    {
+        return reset($this->items);
+    }
+
+    public function next()
+    {
+        return next($this->items);
+    }
+
+    public function current()
+    {
+        return $this->items[$this->key()];
+    }
+
+    public function valid()
+    {
+        return isset($this->items[$this->key()]);
+    }
+
+    public function key()
+    {
+        return key($this->items);
+    }
+
+}
