8ae0fbe3ff93f6c5b29664968d9579061e4a28de
Thibaud Fabre <thibaud@aztech.io>
2014-08-09 11:11:10 +0200

    Add Timer class

---
+++ src/Timer/Timer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Aztech\Util\Timer;
+
+class Timer
+{
+
+    private $started = false;
+
+    private $startTime = 0;
+
+    private $endTime = 0;
+
+    public function start() {
+        $this->started = true;
+        $this->reset();
+    }
+
+    public function stop()
+    {
+        $this->endTime = microtime(true);
+        $this->started = false;
+    }
+
+    public function reset()
+    {
+        $this->startTime = microtime(true);
+    }
+
+    public function getElapsed()
+    {
+        return round($this->endTime - $this->startTime, 3);
+    }
+
+}
