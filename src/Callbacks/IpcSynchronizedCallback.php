d3e617e4da533d123bc330bfb3afdbca0bafe56a
Aztech <thibaud@aztech.io>
2014-08-16 14:15:40 +0200

    WIP

---
+++ src/Callbacks/IpcSynchronizedCallback.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Aztech\Util\Callbacks;
+
+use Aztech\Events\File\Files;
+
+class IpcSynchronizedCallback
+{
+    
+    private $callback;
+    
+    private $hash;
+    
+    private $lockDirectory;
+    
+    private $file;
+    
+    public function __construct($callback, $lockDirectory = '/tmp/')
+    {
+        if (! $callback || ! is_callable($callback)) {
+            throw new \InvalidArgumentException('Parameter is not a valid callback.');
+        }
+        
+        $this->callback = $callback;
+        $this->hash = var_export($this->callback, true);
+        $this->lockDirectory = $lockDirectory;
+    }
+    
+    public function __invoke()
+    {
+        $this->file = fopen($this->lockDirectory . '/' . $this->hash . '.lock', 'c+');
+        
+        if ($this->file) {
+            $result = Files::invokeEx(array($this, 'call'), $handle, func_get_args());
+            
+            fclose($this->file);
+        }
+    }
+    
+    public function call($handle, $args)
+    {
+        return call_user_func_array($this->callback, $args);   
+    }
+}
---
+++ src/File/Files.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aztech\Events\File;
+
+class Files
+{
+
+    /**
+     * Execute a callback in an exclusive manner by attempting to obtain a lock
+     * on the provided file handle 
+     * @param unknown $callback
+     * @param unknown $handle
+     * @return unknown
+     */
+    public static function invokeEx($callback, $handle)
+    {
+        if (flock($handle, LOCK_EX)) {
+            $result = call_user_func_array($callback,  splice(func_get_args(), 1));
+
+            flock($handle, LOCK_UN);
+            
+            return $result;
+        }
+    }
+    
+    public static function readLineEx($handle)
+    {
+        $callback = function($handle) {
+            $content = '';
+            
+            if ($line = fgets($handle)) {
+                return $line;
+            }
+            
+            return null;
+        };
+        
+        return self::invokeEx($callback, $handle);
+    }
+}
